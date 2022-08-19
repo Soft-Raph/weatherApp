a5bb6835fb5f5447b72fea0439aa5c95eb22ca64
soft-raph <ariyoraphael1@gmail.com>
2022-08-19 12:55:30 +0100

    component

---
+++ src/components/BaseModel.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="absolute w-full bg-black bg-opasity-30 h-screen top-0 left-0 flex justify-center px-8">
+       <div class="p-4 bg-white self-start mt-32 max-w-screen-md"></div> 
+       <slot/>
+       <button class="text-white mt-8 bg-weather-primary py-2 px-6">Close</button>
+    </div>
+</template>
+
+<script setup>
+
+</script>
